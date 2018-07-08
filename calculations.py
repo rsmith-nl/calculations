d96434ce7ff8250ec9ee76d1b5e27e8d72075b8d
Roland Smith <rsmith@xs4all.nl>
2018-07-08 10:42:57 +0200

    Convert to f-strings.

--- calculations.py
+++ calculations.py
@@ -4,7 +4,7 @@
 # Copyright © 2017 R.F. Smith <rsmith@xs4all.nl>.
 # SPDX-License-Identifier: MIT
 # Created: 2017-07-09T17:02:43+0200
-# Last modified: 2018-04-17T21:48:54+0200
+# Last modified: 2018-07-08T10:42:19+0200
 """Calculate and print Python expressions."""
 
 _local_dict = {}
@@ -16,7 +16,7 @@ def do(expr, comment='', loc=_local_dict):
     Arguments:
         expr: Assignment expression to print.
         comment: Text to be printed after the assignment.
-        loc: Dictionary of locals. Defaults to local_dict.
+        loc: Dictionary of locals. Defaults to _local_dict.
     """
     before = set(loc.keys())
     exec(expr, None, loc)
@@ -30,7 +30,7 @@ def do(expr, comment='', loc=_local_dict):
     try:
         v = float(expr.split('=')[1])
         del v
-        print('{} {}'.format(expr, comment))
+        print(f'{expr} {comment}')
     except ValueError:
         k = newkeys.pop()
-        print('{} = {:.4g} {}'.format(expr, loc[k], comment))
+        print(f'{expr} = {loc[k]:.4g} {comment}')
